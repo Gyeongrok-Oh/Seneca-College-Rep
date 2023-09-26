790438aaaed90c5f1d773796cc99e6341403dbb6
gyeongrok <0kr3670@gmail.com>
2023-09-25 20:43:30 -0400

    Generate assignment 2 file

---
+++ Semester 3 (fall)/WEB 322/Assignment 2/server.js
@@ -0,0 +1,21 @@
+/********************************************************************************
+
+* WEB322 – Assignment 02
+
+*
+
+* I declare that this assignment is my own work in accordance with Seneca's
+
+* Academic Integrity Policy:
+
+*
+
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+
+*
+
+* Name: Gyeongrok Oh   Student ID: 119140226   Date: ______________
+
+*
+
+********************************************************************************/
